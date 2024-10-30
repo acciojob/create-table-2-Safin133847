bc1a4d511e554282772abf758eb1d0d00c476da9
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2024-10-30 19:52:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
-    //Write your code here
-  
+    const rows = prompt("Input number of rows:");
+    const columns = prompt("Input number of columns:");
+
+    const table = document.getElementById('myTable');
+
+    table.innerHTML = '';
+
+    for (let i = 0; i < rows; i++) {
+        const row = document.createElement('tr');
+
+        for (let j = 0; j < columns; j++) {
+            const cell = document.createElement('td');
+            cell.textContent = `Row-${i} Column-${j}`;
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
 }
